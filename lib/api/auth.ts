b2cc39509edae642445b1228edad0c6da94a9f11
thanhnguyen1502 <thanhlucky150204@gmail.com>
2026-07-22 16:15:01 +0700

    fix: fix UI login and add logic api

--- lib/api/auth.ts
+++ lib/api/auth.ts
@@ -4,8 +4,8 @@ import { User } from "@/types/user";
 import axios from "axios";
 
 export const authApi = {
-    register: (data: RegisterRequest) => client.post<User>('/auth/register', data),
-    login: (data: LoginRequest) => client.post<LoginResponse>('/auth/login', data),
+    register: (data: RegisterRequest) => client.post<User, any>('/auth/register', data),
+    login: (data: LoginRequest) => client.post<LoginResponse, any>('/auth/login', data),
     refresh: (refreshToken: string) =>
         axios.post<{ data: RefreshResponse }>(
             `${process.env.NEXT_PUBLIC_API_URL}/auth/refresh`,
